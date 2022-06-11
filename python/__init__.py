9ab39708bfab54bc543685464758d85323e49d4d
Chris <14341145+cnpryer@users.noreply.github.com>
2022-06-11 18:08:30 -0400

    0.1.1 (#4)

--- python/__init__.py
+++ python/__init__.py
@@ -1,3 +1,3 @@
-from .pylamine import get_sheet_data, get_sheet_names
+from .pylamine import __version__, get_sheet_data, get_sheet_names
 
-__all__ = ["get_sheet_data", "get_sheet_names"]
+__all__ = ["get_sheet_data", "get_sheet_names", "__version__"]
